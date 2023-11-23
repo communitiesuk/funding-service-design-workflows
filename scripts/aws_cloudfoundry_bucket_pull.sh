4258822a59077a3aa35557c5854df850cec6c222
Robert Kibble <robert.kibble@version1.com>
2023-11-23 14:36:16 +0000

    Use yq since we've used that already

--- scripts/aws_cloudfoundry_bucket_pull.sh
+++ scripts/aws_cloudfoundry_bucket_pull.sh
@@ -32,13 +32,13 @@ cf delete-service-key -f "${SERVICE_INSTANCE_NAME}" "${KEY_NAME}"
 cf create-service-key "${SERVICE_INSTANCE_NAME}" "${KEY_NAME}" -c '{"allow_external_access": true}'
 export S3_CREDENTIALS=$(cf service-key "${SERVICE_INSTANCE_NAME}" "${KEY_NAME}" | tail -n +2)
 echo $S3_CREDENTIALS
-echo $S3_CREDENTIALS | jq -r '.credentials'
-echo $S3_CREDENTIALS | jq -r '.credentials.aws_access_key_id'
+echo $S3_CREDENTIALS | yq -r '.credentials'
+echo $S3_CREDENTIALS | yq -r '.credentials.aws_access_key_id'
 
-export AWS_ACCESS_KEY_ID=$(echo "${S3_CREDENTIALS}" | jq -r '.credentials.aws_access_key_id')
-export AWS_SECRET_ACCESS_KEY=$(echo "${S3_CREDENTIALS}" | jq -r '.credentials.aws_secret_access_key')
-export BUCKET_NAME=$(echo "${S3_CREDENTIALS}" | jq -r '.credentials.bucket_name')
-export AWS_DEFAULT_REGION=$(echo "${S3_CREDENTIALS}" | jq -r '.credentials.aws_region')
+export AWS_ACCESS_KEY_ID=$(echo "${S3_CREDENTIALS}" | yq -r '.credentials.aws_access_key_id')
+export AWS_SECRET_ACCESS_KEY=$(echo "${S3_CREDENTIALS}" | yq -r '.credentials.aws_secret_access_key')
+export BUCKET_NAME=$(echo "${S3_CREDENTIALS}" | yq -r '.credentials.bucket_name')
+export AWS_DEFAULT_REGION=$(echo "${S3_CREDENTIALS}" | yq -r '.credentials.aws_region')
 
 env | grep AWS
 aws s3 ls --recursive s3://$BUCKET_NAME | while read d t sz filename
