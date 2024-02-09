727084fbd50bb6a6df03757dbf630eea7e7c8841
Robert Kibble <robert.kibble@version1.com>
2024-02-09 10:50:59 +0000

    Move to this workflow for testing for now

---
+++ scripts/list-versions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd apps/pre-award
+copilot svc ls --json | yq -r '.services[] | select(.app=="pre-award").name' | grep -v exit | while read svc
+do
+  sha=$(copilot svc show -n $svc --json | yq -r '.variables[] | select(.name=="GITHUB_SHA").value')
+  printf "%22s %s\n" $svc $sha
+done
