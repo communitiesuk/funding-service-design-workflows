002a2a6c7bde3565ef21cad159e77c974c14f87c
Samuel Williams <samuelhwilliams@gmail.com>
2024-07-18 16:03:48 +0100

    Add before/after rowcount validation

--- scripts/aws_db_dump_restore.sh
+++ scripts/aws_db_dump_restore.sh
@@ -44,8 +44,21 @@ aws ssm start-session --target $TARGET_BASTION --document-name AWS-StartPortForw
 sleep 5
 
 SOURCE_URL=postgres://$SOURCE_USERNAME:$SOURCE_PASSWORD@localhost:$SOURCE_LPORT/$SOURCE_DBNAME
-TARGET_URL=postgres://$TARGET_USERNAME:$TARGET_PASSWORD@localhost:$TARGET_LPORT/$TARGET_DBNAME
+TARGET_URL=postgres://$TARGET_USERNAME:$TARGET_PASSWORD@localhost:$TARGET_LPORT/postgres
+TARGET_URL_DATA_API_DB=postgres://$TARGET_USERNAME:$TARGET_PASSWORD@localhost:$TARGET_LPORT/$TARGET_DBNAME
+
+psql ${SOURCE_URL} <<EOF > source_count.txt
+select 'source', 0 union all select 'alembic_version', count(*) from alembic_version union all select 'fund_dim', count(*) from fund_dim union all select 'funding', count(*) from funding union all select 'funding_comment', count(*) from funding_comment union all select 'funding_question', count(*) from funding_question union all select 'geospatial_dim', count(*) from geospatial_dim union all select 'organisation_dim', count(*) from organisation_dim union all select 'outcome_data', count(*) from outcome_data union all select 'outcome_dim', count(*) from outcome_dim union all select 'output_data', count(*) from output_data union all select 'output_dim', count(*) from output_dim union all select 'place_detail', count(*) from place_detail union all select 'private_investment', count(*) from private_investment union all select 'programme_dim', count(*) from programme_dim union all select 'programme_funding_management', count(*) from programme_funding_management union all select 'programme_junction', count(*) from programme_junction union all select 'programme_progress', count(*) from programme_progress union all select 'project_dim', count(*) from project_dim union all select 'project_finance_change', count(*) from project_finance_change union all select 'project_geospatial_association', count(*) from project_geospatial_association union all select 'project_progress', count(*) from project_progress union all select 'risk_register', count(*) from risk_register union all select 'submission_dim', count(*) from submission_dim ORDER BY 1
+EOF
 
 echo "Doing dump and restore..."
-pg_dump $SOURCE_URL -v -F c --clean | pg_restore -d $TARGET_URL -v --clean
+pg_dump $SOURCE_URL -v -F c --clean --create | pg_restore -d $TARGET_URL -v --clean --create
+psql ${TARGET_URL} -c "ANALYZE;"
 echo "Completed dump and restore."
+
+psql ${TARGET_URL_DATA_API_DB} <<EOF > target_count.txt
+select 'target', 0 union all select 'alembic_version', count(*) from alembic_version union all select 'fund_dim', count(*) from fund_dim union all select 'funding', count(*) from funding union all select 'funding_comment', count(*) from funding_comment union all select 'funding_question', count(*) from funding_question union all select 'geospatial_dim', count(*) from geospatial_dim union all select 'organisation_dim', count(*) from organisation_dim union all select 'outcome_data', count(*) from outcome_data union all select 'outcome_dim', count(*) from outcome_dim union all select 'output_data', count(*) from output_data union all select 'output_dim', count(*) from output_dim union all select 'place_detail', count(*) from place_detail union all select 'private_investment', count(*) from private_investment union all select 'programme_dim', count(*) from programme_dim union all select 'programme_funding_management', count(*) from programme_funding_management union all select 'programme_junction', count(*) from programme_junction union all select 'programme_progress', count(*) from programme_progress union all select 'project_dim', count(*) from project_dim union all select 'project_finance_change', count(*) from project_finance_change union all select 'project_geospatial_association', count(*) from project_geospatial_association union all select 'project_progress', count(*) from project_progress union all select 'risk_register', count(*) from risk_register union all select 'submission_dim', count(*) from submission_dim ORDER BY 1
+EOF
+
+echo "Analysing diffs between source DB and target DB row counts"
+diff source_count.txt target_count.txt
